b8763028385d22bbb325975a810fb39bc481ca31
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-05-09 11:51:59 +0530

    updated modules

--- client/src/components/macro components/Track/CalendarTrack.jsx
+++ client/src/components/macro components/Track/CalendarTrack.jsx
@@ -4,17 +4,46 @@ import css from "../../../styles/Track/CalendarTrack.module.css";
 
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
-
 function CalendarTrack() {
+
+  const Calevents = [
+    {
+      title: 'Event 1',
+      start: '2023-05-15T10:00:00',
+      end: '2023-05-15T12:00:00',
+    },
+    {
+      title: 'Event 4',
+      start: '2023-05-15T10:00:00',
+      end: '2023-05-15T13:00:00',
+    },
+    {
+      title: 'Event 2',
+      start: '2023-05-16T14:00:00',
+      end: '2023-05-16T16:00:00',
+    },
+    {
+      title: 'Event 3',
+      start: '2023-05-17T09:00:00',
+      end: '2023-05-17T11:00:00',
+    },
+  ];
+
+  const handleEventClick = (info) => {
+    console.log(info.event);
+  };
+
+
   return (
-    <div>
-      Calender
+    <div className={css.calendarConatienr}>
+      <FullCalendar 
+       plugins={[dayGridPlugin,interactionPlugin]}
+       initialView='dayGridMonth'
+       weekends = {true}
+       events={Calevents}
+       eventClick={handleEventClick} />
     </div>
   );
 }
