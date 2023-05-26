ef3f2054a6f233a27b78bd16075b473c4481088d
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-05-26 15:49:47 +0530

    added new end points

--- server/src/Controllers/userController.js
+++ server/src/Controllers/userController.js
@@ -1,11 +1,12 @@
 const userModel = require("../Models/user");
+const verifyModel  = require("../Models/verificationCode");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = "S3CR3T_T@K3R_@0863";
 const sgMail = require('@sendgrid/mail');
 
 // Set SendGrid API key
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SECRET_SENDGRID_API_KEY);
 
 const signup = async (req,res) =>{
     // existing user check
@@ -74,29 +75,48 @@ const signin = async (req,res) =>{
     
 }
 const sendVerificationCode = async (req,res)=>{
+      // In this function we will store verify code in the database temporarily
+      // then send this code to the email 
       const userEmail = req.body.email;
       const verificationCode = Math.floor(100000 + Math.random() * 900000);
       try {
-            const existinguser = await userModel.findOne({email:userEmail});
-            if(!existinguser){
-                return res.status(404).json({message : "User not exists"})
-            }
-
+            const verifyUser = await verifyModel.create({
+                email: userEmail,
+                code: verificationCode,
+                expirationTimestamp: new Date(new Date().getTime() + 10 * 60 * 1000) // code expires in 10 mins
+            })
+            // sending code to email
             const msg = {
                 to : userEmail,
                 from: "altairFarooqueDeveloper@gmail.com",
                 subject: 'Verification Code',
-                text: `Your verification code is : ${verificationCode}`,
+                text: `Your verification code is : ${verificationCode}  \n this code expires in 10 mins`,
             }
-            const sendCode = sgMail.send(msg);
-            res.status(200).json({ message :"Verification code send Succefuuly"})
+            const sendCode = await sgMail.send(msg);
+            res.status(200).json({ message :"Verification code send Succefuuly", sendCode:sendCode})
       }catch (error) {
         console.log(error);
         res.status(500).json({message:"Error while sending code!"})
       }
-
 }
 const verifyCodeFromClient = async (req,res) =>{
+      const { email, userCode }= res.body;
+      try {
+        const result  = await verifyModel.findOne({email:email});
+        if(result.code === userCode){
+            res.status(200).json({message:"Verification Successful" , verifyStatus : "0001"});
+        }
+        else{
+            res.status(404).json({
+                message : "Inavlid code",
+                verifyStatus : "0000"
+            });
+        }
+        
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Error while verifying code!"})
+      }
       
 }
 
