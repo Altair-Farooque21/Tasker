0f0c43b9cd3d3f74c126ee3b0db896476513ed37
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-05-27 11:35:45 +0530

    verification end points added

--- server/src/Controllers/userController.js
+++ server/src/Controllers/userController.js
@@ -74,12 +74,14 @@ const signin = async (req,res) =>{
     }
     
 }
+
 const sendVerificationCode = async (req,res)=>{
       // In this function we will store verify code in the database temporarily
       // then send this code to the email 
       const userEmail = req.body.email;
       const verificationCode = Math.floor(100000 + Math.random() * 900000);
       try {
+           // verified database entry ✅
             const verifyUser = await verifyModel.create({
                 email: userEmail,
                 code: verificationCode,
@@ -88,36 +90,64 @@ const sendVerificationCode = async (req,res)=>{
             // sending code to email
             const msg = {
                 to : userEmail,
-                from: "altairFarooqueDeveloper@gmail.com",
+                from: {email:"altairfarooquedeveloper@gmail.com"},
                 subject: 'Verification Code',
                 text: `Your verification code is : ${verificationCode}  \n this code expires in 10 mins`,
             }
             const sendCode = await sgMail.send(msg);
-            res.status(200).json({ message :"Verification code send Succefuuly", sendCode:sendCode})
+            res.status(200).json({ message :"Verification code send Succefuuly",verifyStatus : "1001",})
       }catch (error) {
         console.log(error);
         res.status(500).json({message:"Error while sending code!"})
       }
 }
+
+const resendVerificationCode = async (req,res)  =>{
+      // in this filed we will try finding 
+      // get existing verification code
+      const existEmail = req.body.email
+      const newCode = Math.floor(100000 + Math.random() * 900000);
+      try {
+           const existCode = verifyModel.findOne({email:existEmail});
+           // since its resend code we can send existing one or 
+           // new code and send to the mail           
+           existCode.code = newCode;
+           // this step will renew the existing code expiration time ,since
+           // we are requesting new code we would like to start new expiration too
+           existCode.expirationTimestamp = new Date(new Date().getTime() + 10 * 60 * 1000);
+           existCode.save();
+           const msg = {
+            to : existEmail,
+            from: {email:"altairfarooquedeveloper@gmail.com"},
+            subject: 'Resend Verification Code',
+            text: `Your verification code is : ${newCode}  \n this code expires in 10 mins`,
+                        }
+            const sendCode = await sgMail.send(msg);
+            res.status(200).json({ message :"Verification code send Succefuuly",verifyStatus : "2002",})
+
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Error while sending code!"})
+      }
+}
 const verifyCodeFromClient = async (req,res) =>{
-      const { email, userCode }= res.body;
+      const { email, userCode }= req.body;
       try {
         const result  = await verifyModel.findOne({email:email});
         if(result.code === userCode){
-            res.status(200).json({message:"Verification Successful" , verifyStatus : "0001"});
+            res.status(200).json({message:"Verification Successful" , verifyStatus : "0001",result:result});
         }
         else{
             res.status(404).json({
                 message : "Inavlid code",
-                verifyStatus : "0000"
+                verifyStatus : "0000",
+                result:result
             });
         }
-        
       } catch (error) {
         console.log(error);
         res.status(500).json({message:"Error while verifying code!"})
       }
-      
 }
 
-module.exports = {signin,signup,verifyCodeFromClient,sendVerificationCode};
+module.exports = {signin,signup,verifyCodeFromClient,sendVerificationCode,resendVerificationCode};
--- server/src/Routes/userRoutes.js
+++ server/src/Routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const {signin,signup,verifyCodeFromClient,sendVerificationCode}= require('../Controllers/userController');
+const {signin,
+       signup,
+       verifyCodeFromClient,
+       sendVerificationCode,
+       resendVerificationCode}= require('../Controllers/userController');
 const userRouter = express.Router();
 
 userRouter.post('/signup',signup);
@@ -7,5 +11,9 @@ userRouter.post('/signup',signup);
 userRouter.post('/signin',signin);
 
 userRouter.post('/send-verification-code',sendVerificationCode);
+
+userRouter.post('/code-verfication',verifyCodeFromClient);
+
+userRouter.post('/resend-code',resendVerificationCode);`` 
 
 module.exports = userRouter;
